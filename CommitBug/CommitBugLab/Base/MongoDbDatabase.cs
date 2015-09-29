bfab3f6081895e8a1fb0b00d74edf5936c60be7c
inernoro <1025508785@qq.com>
2015-09-29 19:44:14 +0800

    add commit

---
+++ CommitBug/CommitBugLab/Base/MongoDbDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using MongoDB.Bson;
+using System.Configuration;
+
+namespace CommitBugLab.Base
+{
+    public class MongoDbDatabase : IDatabase
+    {
+
+        public string GetDataAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MongoSetData(BugModel model)
+        {
+            MongodbHelper h = new MongodbHelper();
+
+            model.BrowserInfo = JsonConvert.SerializeObject(model.BrowserInfo);
+            model.exception = JsonConvert.SerializeObject(model.exception);
+            var task = h.Add(model);
+            //task.Wait();
+        }
+
+        public void SetData(string text)
+        {
+
+
+        }
+    }
+}
